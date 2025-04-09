ad4f31f1c16bd37f5e99e86165896589145e8db5
CapronLee <kasamata2.2003@gmail.com>
2025-04-10 03:56:17 +0700

    thêm chức năng gửi ảnh

--- ESSgrading/app/src/main/java/com/example/essgrading/API/ApiService.java
+++ ESSgrading/app/src/main/java/com/example/essgrading/API/ApiService.java
@@ -4,14 +4,25 @@ import com.example.essgrading.Model.ScoreModel;
 import com.example.essgrading.Model.TestModel;
 
 import java.util.List;
+
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 public interface ApiService {
+
     @GET("api/Analysis/{examId}")
     Call<List<ScoreModel>> getScoreReport(@Path("examId") int examId);
 
     @GET("api/Exam/allexam")
     Call<List<TestModel>> getAllExam();
+
+    @Multipart
+    @POST("api/upload/image") //sửa endpoint theo đúng URL backend nhận ảnh
+    Call<ResponseBody> uploadImage(@Part MultipartBody.Part image);
 }
