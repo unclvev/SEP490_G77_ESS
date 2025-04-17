bc615a152ac378bc061f94f3ec87560e436b7320
Nguyễn Thành Đức <nguyenthanhduc208@gmail.com>
2025-04-17 13:07:11 +0700

    done code test for common function

---
+++ SourceCode/BackEnd/SEP490_G77_ESS/SEP490_G77_ESS.Tests/Controller/Analysis/AnalysisControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEP490_G77_ESS.Tests.Controller.Analysis
+{
+    internal class AnalysisControllerTests
+    {
+    }
+}
